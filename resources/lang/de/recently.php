2ef4637eadcde2b59768db8380700d7f0fdc43c8
M. D. <dissto@users.noreply.github.com>
2024-10-02 11:26:11 +0200

    Add german translation

---
+++ resources/lang/de/recently.php
@@ -0,0 +1,9 @@
+<?php
+
+// translations for Awcodes/Recently
+return [
+    'trigger_label' => 'Letzte Datensätze',
+    'no_records' => 'Keine Datensätze gefunden',
+    'clear_records' => 'Verlauf löschen',
+    'global_search_label' => 'Kürzlich',
+];
